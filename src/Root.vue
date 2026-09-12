83fa208d0457ca925124aa604fa4861f76d21f0a
Marco Antônio da Silva Júnior <marco.junior@melhorenvio.com>
2026-09-12 02:40:17 -0300

    feat(logs): mount action history panel

--- src/Root.vue
+++ src/Root.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import App from './App.vue'
+import ActivityLogPanel from './components/ActivityLogPanel.vue'
 import OpenXrManager from './components/OpenXrManager.vue'
 </script>
 
 <template>
   <App />
+  <ActivityLogPanel />
   <OpenXrManager />
 </template>
